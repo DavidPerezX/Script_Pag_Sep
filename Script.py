e7bbacebb693f39f4faff4defeec09e01167b7bf
DavidPerezX <87737711+DavidPerezX@users.noreply.github.com>
2023-08-22 18:03:09 -0600

    Update Script.py
    Se reemplazo el código por su versión asíncrona

--- Script.py
+++ Script.py
@@ -1,69 +1,66 @@
 import os
-import requests
+import asyncio
+import aiohttp
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 # tipos de libros
-tag_books = ('LMP','MLA','PAA','PCA','PEA','SDA','TPA','CMA','SHA')
+tag_books = ('LMP', 'MLA', 'PAA', 'PCA', 'PEA', 'SDA', 'TPA', 'CMA', 'SHA')
 
-#URL bases para la descarga de las imagenes y la busqueda del libro
+# URL bases para la descarga de las imagenes y la busqueda del libro
 base_url = 'https://www.conaliteg.sep.gob.mx/2023/c/'
 base2_url = 'https://www.conaliteg.sep.gob.mx/2023/'
 
-#Contador para P
-for i in range (0,8):
-    #Contador para los Titulos de los libros    
-    for le in range (0,len(tag_books)):
-        #Try y cach por si no encuentra la pagina
-        try:
-            # URL de los libros
-            url =f'{base_url}P{i}{tag_books[le]}/'
-            #url para buscar si existe el libro
-            urlbusqueda =f'{base2_url}P{i}{tag_books[le]}.htm#page/2'
-                        
-            # Realizar la solicitud HTTP a la página web
-            response = requests.get(urlbusqueda)
-            response.raise_for_status()
+async def download_image(session, img_url, img_path):
+    try:
+        async with session.get(img_url) as response:
+            if response.status == 200:
+                with open(img_path, 'wb') as img_file:
+                    img_file.write(await response.read())
+                print(f'Imagen descargada: {img_path}')
+    except Exception as e:
+        print(f'Error al descargar imagen {img_url}: {e}')
 
-            # Analizar el contenido HTML utilizando BeautifulSoup
-            soup = BeautifulSoup(response.content, 'html.parser')
+async def download_books():
+    for i in range(0, 8):
+        for le in range(0, len(tag_books)):
+            try:
+                # URL de los libros
+                url = f'{base_url}P{i}{tag_books[le]}/'
+                # url para buscar si existe el libro
+                urlbusqueda = f'{base2_url}P{i}{tag_books[le]}.htm#page/2'
 
-            #INTENTA ENCONTRAR LAS IMAGENES, DA OTRA COSA 
-            img_tags = soup.find_all('img')
+                async with aiohttp.ClientSession() as session:
+                    response = await session.get(urlbusqueda)
+                    response.raise_for_status()
+                    soup = BeautifulSoup(await response.text(), 'html.parser')
 
-            #Donde se va a guardar
-            output_folder = f'P{i}{tag_books[le]}'
-            if not os.path.exists(output_folder):
-                os.makedirs(output_folder)
+                    img_tags = soup.find_all('img')
 
-            absolute_path = os.path.abspath(output_folder)
+                    output_folder = f'P{i}{tag_books[le]}'
+                    if not os.path.exists(output_folder):
+                        os.makedirs(output_folder)
 
-            #Base para el numero de pagina
-            base = "000"  
+                    base = "000"
 
-            # Descargar las imágenes
-            for img_cont in range (1,400):
-                img_num = f"{base}{img_cont}"[-len(base):]  # Asegura que solo los últimos caracteres se conserven
-                # Obtener la URL de la imagen
-                img_url =f'{url}{img_num}.jpg'
-                img_name = img_url.split('/')[-1]  # Obtener el nombre de archivo de la URL
-                img_path = os.path.join(output_folder, img_name)  # Ruta completa de destino
-                
-                
-                response = requests.get(img_url)  # Realizar la solicitud HTTP
+                    tasks = []
+                    for img_cont in range(1, 400):
+                        img_num = f"{base}{img_cont}"[-len(base):]
+                        img_url = f'{url}{img_num}.jpg'
+                        img_name = img_url.split('/')[-1]
+                        img_path = os.path.join(output_folder, img_name)
 
+                        task = download_image(session, img_url, img_path)
+                        tasks.append(task)
 
-                if response.status_code == 200:
-                    with open(img_path, 'wb') as img_file:
-                        img_file.write(response.content)
-                else:
-                    break
-                
-                print(f'Imagen descargada: {img_name}')
+                    await asyncio.gather(*tasks)
 
-        except requests.exceptions.RequestException as e:
-            print(f'No existe el libro {url}: {e}')
-            continue
+            except aiohttp.ClientError as e:
+                print(f'No existe el libro {url}: {e}')
+                continue
 
-        print('Descarga completada.')
-        print(f'Ruta absoluta de la carpeta de imágenes descargadas: {absolute_path}')
+            print('Descarga completada.')
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(download_books())
